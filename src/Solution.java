272d570bf7f31230ca40777bc711a08e4cb66b5f
arlannomad <arlannomad@gmail.com>
2022-12-03 11:52:30 +0600

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+
+class Solution {
+
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime1 = ZonedDateTime.parse("");
+
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+    }
+
+}
